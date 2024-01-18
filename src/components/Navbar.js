6fd418afb3a440dee8f5f326590f167791ea148f
Suryansh Singh Bisen <07suryansh@gmail.com>
2024-01-18 23:25:03 +0530

    completed title bar and created navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <li>Country</li>
+        <li>Breaking</li>
+        <li>Sports</li>
+        <li>Politics</li>
+        <li>Education</li>
+        <li>Viral</li>
+        <li>Economics</li>
+        <li>Entertainment</li>
+    </div>
+  )
+}
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './title.css'
+
+export default function Title() {
+  return (
+    <div className='title'>
+        <span className='title-initial'>UT</span>
+        <span className='title-name'>UNIVERSE TIMES</span>
+    </div>
+  )
+}
